fa839303feb8c6be3c6d8d28582676ae0c5ca979
Neha Khetan <khetanneha@gmail.com>
2024-04-14 09:32:40 -0700

    Updated readme

--- SIV-TIP-MCMC/extractExptData.py
+++ SIV-TIP-MCMC/extractExptData.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 """Reads data from csv files for All animals: PVL ( ddPCT and qPCR ) , TIP-GFP (ddPCR) and Integrated-TIP, CD4+
    Experiments @ OHSU and Weinberger Lab, 
-   Raw Files from Fathima, Pitchai
+   Raw Files from Fathima Pitchai
 """
 
 def get_dataAdult( dataset , anim2ignore ):
